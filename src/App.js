7a0da2ca739dba4bc2cf676f38cf893c50d039c5
flimflamphlegm <ivanyvr@gmail.com>
2022-03-15 17:14:50 -0700

    dong

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import logo from './profile.jpeg';
 import './App.css';
+import Pdf from './resume.pdf';
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+import { ImageBackground, Image, View, StyleSheet } from 'react-native';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<View style={styles.container}>
+		<View style={styles.item}>
+			<div className='container'>
+				<div className="App">
+				  <header className="App-header">
+					<h1> 
+						Ivan Cheng
+					</h1>
+					<img src={logo} className="pic" alt="me" />
+					<p>
+					I'm a 4th year Electrical Engineering student currently studying at UBC.
+				</p>
+				<a href={Pdf} without rel="noopener noreferrer" target="_blank">
+				  <button className = "button" trailingIcon="picture_as_pdf" label="Resume">
+					Resume
+				  </button>
+			   </a>
+				  </header>
+				  
+				</div>
+				
+		   </div>
+		</View>
+	</View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start' // if you want to fill rows left to right
+  },
+  item: {
+    width: '100%' // is 50% of container width
+  }
+})
